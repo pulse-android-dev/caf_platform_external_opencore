a104ee28aac2464a6781bc6091084a1f77340834
Ravi Kumar Alamanda <ralama@codeaurora.org>
2010-07-26 19:36:49 -0700

    OpenCore: Fix for AV sync issue when video playback is resumed
    - When a video playback is resumed, there is no need to check for
      delayed start, which could adjust the clock and cause AV sync
      loss
    - Modified not to check for delayed start incase of resuming
      video playback or repositioning
    
    Change-Id: I331a4d8ad591de305ce4148e0331078a6189d582
    CRs-fixed: 248413

--- android/android_audio_mio.h
+++ android/android_audio_mio.h
@@ -135,6 +135,7 @@ public:
     void incFrameCount(uint32_t numFrames) { iFrameCount += numFrames; }
     void setFrameRate(float msecsPerFrame) { iMsecsPerFrame = msecsPerFrame; }
     float msecsPerFrame() { return iMsecsPerFrame; }
+    void checkDelayedStart(bool flag) {checkForDelayedStart = !flag;}
 
 private:
     PVMFMediaClock* iClock;
